f75bec7fb1daffe98baff9561d85efb782665f51
Johnathan Tenny <107655580+j-tenny@users.noreply.github.com>
2025-01-28 18:41:58 -0700

    pip install working

---
+++ nfdrs4py/__init__.py
@@ -0,0 +1,11 @@
+from .nfdrs4_bindings import NFDRS4, DeadFuelMoisture, LiveFuelMoisture, CNFDRSParams, CDeadFuelMoistureParams, CGSIParams, CFuelModelParams
+
+__all__ = [
+    "NFDRS4",
+    "DeadFuelMoisture",
+    "LiveFuelMoisture",
+    "CNFDRSParams",
+    "CDeadFuelMoistureParams",
+    "CGSIParams",
+    "CFuelModelParams",
+]
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-
+from setuptools.command.build_py import build_py
+from setuptools import find_packages
 # Define the SWIG extension
 swig_extension = Extension(
-    name='_nfdrs4',  # Name of the Python package
-    sources=['nfdrs4.i', 'lib/NFDRS4/src/deadfuelmoisture.cpp',  'lib/NFDRS4/src/livefuelmoisture.cpp',
+    name='_nfdrs4_bindings',  # Name of the Python package
+    sources=['nfdrs4py/nfdrs4.i', 'lib/NFDRS4/src/deadfuelmoisture.cpp',  'lib/NFDRS4/src/livefuelmoisture.cpp',
              'lib/NFDRS4/src/dfmcalcstate.cpp', 'lib/NFDRS4/src/lfmcalcstate.cpp','lib/NFDRS4/src/nfdrs4calcstate.cpp',
              'lib/NFDRS4/src/nfdrs4.cpp', 'lib/NFDRS4/src/CNFDRSParams.cpp', 'lib/utctime/src/utctime.cpp', 'lib/time64/src/time64.c'],
     swig_opts=['-c++'],  # SWIG options
@@ -14,10 +15,19 @@ swig_extension = Extension(
     extra_link_args=[],  # Additional linker options
 )
 
+class BuildPy(build_py):
+    def run(self):
+        self.run_command('build_ext')
+        super(build_py, self).run()
+
 setup(
-    name='nfdrs4',
+    name='nfdrs4py',
     version='0.1',
-    description='Python package using SWIG and C++',
+    description='Python interface to NFDRS',
     ext_modules=[swig_extension],
-    cmdclass={'build_ext': build_ext},
+    cmdclass={
+        'build_py': BuildPy,
+    },
+    packages=find_packages('nfdrs4py'),
+    package_dir={'': 'nfdrs4py'}
 )
