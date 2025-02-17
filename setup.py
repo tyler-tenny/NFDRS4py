83539aff3950d66d44998bfd2d98bf02a46b077b
Tyler Tenny <tyler.tenny@oit.edu>
2025-02-17 01:51:03 -0800

    use g++

--- setup.py
+++ setup.py
@@ -4,18 +4,35 @@ from setuptools.command.build_py import build_py
 from setuptools import find_packages
 import numpy
 
+# Split c and c++ files
+c_sources = ['lib/time64/src/time64.c']
+cpp_sources = ['nfdrs4py/nfdrs4.i', 'lib/NFDRS4/src/deadfuelmoisture.cpp',  'lib/NFDRS4/src/livefuelmoisture.cpp',
+             'lib/NFDRS4/src/dfmcalcstate.cpp', 'lib/NFDRS4/src/lfmcalcstate.cpp','lib/NFDRS4/src/nfdrs4calcstate.cpp',
+             'lib/NFDRS4/src/nfdrs4.cpp', 'lib/NFDRS4/src/CNFDRSParams.cpp', 'lib/utctime/src/utctime.cpp',]
 # Define the SWIG extension
 swig_extension = Extension(
     name='nfdrs4py._nfdrs4_bindings',  # Name of the Python package
-    sources=['nfdrs4py/nfdrs4.i', 'lib/NFDRS4/src/deadfuelmoisture.cpp',  'lib/NFDRS4/src/livefuelmoisture.cpp',
-             'lib/NFDRS4/src/dfmcalcstate.cpp', 'lib/NFDRS4/src/lfmcalcstate.cpp','lib/NFDRS4/src/nfdrs4calcstate.cpp',
-             'lib/NFDRS4/src/nfdrs4.cpp', 'lib/NFDRS4/src/CNFDRSParams.cpp', 'lib/utctime/src/utctime.cpp', 'lib/time64/src/time64.c'],
+    sources=c_sources + cpp_sources,
     swig_opts=['-c++'],  # SWIG options
     language='c++',
     include_dirs=['lib/NFDRS4/include','lib/time64/include','lib/utctime/include',numpy.get_include()],  # C:/Users/john1/miniforge3/include/
     extra_compile_args=['-std=c++11'],  # Additional compiler options
     extra_link_args=[],  # Additional linker options
 )
+
+class CCustomBuildExt(build_ext):
+    def build_extension(self, ext):
+        c_files = [f for f in ext.sources if f.endswith('.c')]
+        cpp_files = [f for f in ext.sources if f.endswith('.cpp')]
+
+        if c_files:
+            self.compiler.compiler_so[0] = 'gcc'
+            ext.sources = c_files
+            super().build_extension(ext)
+        if cpp_files:
+            self.compiler.compiler_so[0] = 'g++'
+            ext.sources = cpp_files
+            super().build_extension_ext
 
 class BuildPy(build_py):
     def run(self):
